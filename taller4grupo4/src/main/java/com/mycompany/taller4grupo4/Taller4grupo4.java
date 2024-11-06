a3aceabe31acb4777b551d1eb6132d3cb1d48e3d
Marlo Valle Franco <mcvalle@espol.edu.ec>
2024-11-06 14:13:49 -0500

    Subido el proyecto
    marcitoooo

---
+++ taller4grupo4/src/main/java/com/mycompany/taller4grupo4/Taller4grupo4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller4grupo4;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller4grupo4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
