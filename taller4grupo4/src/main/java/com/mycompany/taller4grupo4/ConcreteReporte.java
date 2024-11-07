66d9ccb141f21a9e7f959ac440421073e0de9120
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-06 23:35:25 -0500

    Update ConcreteReporte.java

--- taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteReporte.java
+++ taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteReporte.java
@@ -1,3 +1,9 @@
-public class ConcreteReporte {
+package com.mycompany.taller4grupo4;
+public class ConcreteReporte implements Reporte {
+    
+    @Override
+    public void escribir() {
+        System.out.println("Escribiendo el reporte en el formato especificado...");
+    }
 
 }
