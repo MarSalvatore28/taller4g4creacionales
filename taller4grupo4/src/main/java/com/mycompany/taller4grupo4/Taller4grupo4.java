75a57b45942abc0fc9298a9bfa9c2128312c72ab
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-06 21:58:08 -0500

    Update Taller4grupo4.java

--- taller4grupo4/src/main/java/com/mycompany/taller4grupo4/Taller4grupo4.java
+++ taller4grupo4/src/main/java/com/mycompany/taller4grupo4/Taller4grupo4.java
@@ -11,8 +11,28 @@ package com.mycompany.taller4grupo4;
 public class Taller4grupo4 {
 
     public static void main(String[] args) {
+
         Correo correo = new Correo();
         Notificacion adaptadorCorreo = new CorreoAdaptador(correo);
         adaptadorCorreo.mandarNotificacion("Este es un mensaje de prueba.");
+
+
+        ConcreteCreator creator = ConcreteCreator.getInstance();
+        
+        // Crear un reporte concreto esto lo usamos para tener un reporte en especial 
+        Reporte reporte = creator.crearConcrete();
+        // Usamos el reporte concreto que queriamos y habiamos creado 
+        reporte.escribir();
+
+
+
+
+
+
+
+
+
+
+
     }
 }
