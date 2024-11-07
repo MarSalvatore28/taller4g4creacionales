6cc3fd829e27ffb1effc9ccb4f971e218454ddea
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-06 23:02:40 -0500

    Update ConcreteCreator.java

--- taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteCreator.java
+++ taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteCreator.java
@@ -1,5 +1,28 @@
 package com.mycompany.taller4grupo4;
 
-public class ConcreteCreator {
+public class ConcreteCreator implements Creator , Reporte {
+    private static ConcreteCreator instance;
+
+    private ConcreteCreator() {
+        // Constructor privado para prevenir instanciación
+    }
+
+    // Método para obtener la instancia única
+    public static ConcreteCreator getInstance() {
+        if (instance == null) {
+            instance = new ConcreteCreator();
+        }
+        return instance;
+    }
+
+    @Override
+    public Reporte crearConcrete() {
+        return new ConcreteReporte();
+    }
+
+    @Override
+    public void escribir() {
+        System.out.println("Escribiendo el contenido del reporte básico.");
+    }
 
 }
