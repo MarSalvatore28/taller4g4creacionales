92393313ecd8734353fbe7f4d94c0fb4b454f4c9
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-06 23:33:44 -0500

    Update ConcreteDecoratorA.java

--- taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteDecoratorA.java
+++ taller4grupo4/src/main/java/com/mycompany/taller4grupo4/ConcreteDecoratorA.java
@@ -1,5 +1,18 @@
 package com.mycompany.taller4grupo4;
 
-public class ConcreteDecoratorA {
+public class ConcreteDecoratorA extends Decorator {
+    public ConcreteDecoratorA(Reporte reporte) {
+        super(reporte);
+    }
+
+    @Override
+    public void escribir() {
+        super.escribir();
+        agregarEstilo();
+    }
+
+    private void agregarEstilo() {
+        System.out.println("Agregando estilo adicional al reporte.");
+    }
 
 }
